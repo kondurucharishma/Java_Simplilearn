cf279a0b2c6f8f65a7aae2deb5d91b79f4e559a7
charishma <kondurucharishma369@gmail.com>
2022-02-26 18:22:40 +0530

    add java

---
+++ myfirstproject/src/com/mphasis/demo/Demo.java
@@ -0,0 +1,18 @@
+package com.mphasis.demo;
+
+import java.util.Iterator;
+
+public class Demo {
+
+	public static void main(String[] args) {
+    Demo demo = new Demo();
+		String str = new String("charishma");
+
+			
+	
+		
+		System.out.println(demo);
+		System.out.println(str);
+	}
+
+}
